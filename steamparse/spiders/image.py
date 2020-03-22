b6d735a74f2535a32946b2db9f10a6f7e9da8d49
davitk <dkoshkeli@gmail.com>
2020-03-22 15:07:17 +0400

    fixed self.parse_after issue

--- steamparse/spiders/image.py
+++ steamparse/spiders/image.py
@@ -10,11 +10,13 @@ IMG_XP = '//div[1]/div[7]/div[3]/div/div[1]/div/div/div/div[2]/div/img/@src'
 BASE_URL = 'https://steamcommunity.com/profiles/'
 MIDDLE_NUMBER = '76561198'
 
+QUESTIONMARK_HASH = 'a454f00ccb8f2f1ccf7c08de679700879cf5da996df27362cd9df44137b80cd1'
+
 HEADERS = {"user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36"}
 
 class Spider(scrapy.Spider):
     name = 'image'
-    allowed_domains = ['steamcommunity.com']
+    allowed_domains = ['steamcommunity.com', 'steamcdn-a.akamaihd.net']
     start_urls = []
 
     def start_requests(self):
@@ -22,22 +24,24 @@ class Spider(scrapy.Spider):
             ending_number = ''.join(map(str, comb))[::-1]
             ending_number = MIDDLE_NUMBER + ending_number
             url = BASE_URL + ending_number
-            request = Request(url=url, callback=self.parse, meta={"steamid": ending_number})
+            request = Request(url=url, callback=self.parse, meta={"steam_id": ending_number})
             yield request
 
     def parse(self, response):
         image = response.xpath(IMG_XP).extract_first()
         red_text = response.xpath(RED_TEXT_XP).extract()
-        _id = response.meta.get('steamid')
         if not red_text and image:
+            _id = response.meta.get('steam_id')
             image = image.split('_full')[0] + '.jpg'
             yield Request(url=image, callback=self.parse_after, headers=HEADERS,
-                          meta={"image": image,
-                                "steam_id": _id})
+                          meta={"steam_id": _id,
+                                "steam_profile": response.url})
 
     def parse_after(self, response):
-        context = RaviItem()
-        context['index'] = sha256(response.content[:500])
-        context['image'] = response.meta.get("image")
-        context['steam_id'] = response.meta.get("steam_id")
-        return context
+        img_hash = sha256(response.body[:500])
+        if img_hash != QUESTIONMARK_HASH:
+            context = RaviItem()
+            context['_id'] = response.meta.get("steam_id")
+            context['image_hash'] = img_hash.hexdigest()
+            context['steam_profile'] = response.meta.get("steam_profile")
+            return context
